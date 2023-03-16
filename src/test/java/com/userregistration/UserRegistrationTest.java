171142b1065e7d00a4fb9320e58eef5e0e9950b8
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-16 18:43:03 +0530

    REFACTORED : code for happy and sad cases

--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -13,10 +13,24 @@ public class UserRegistrationTest {
     }
 
     @Test
+    public void givenFirstName_IfInvalid_ShouldReturnFalse(){
+        UserRegistration registration = new UserRegistration();
+        boolean result=registration.checkFirstName("kartikeya");
+        Assert.assertFalse(result);
+    }
+
+    @Test
     public void givenLastName_IfValid_ShouldReturnTrue(){
         UserRegistration registration = new UserRegistration();
         boolean result=registration.checkLastName("Maan");
         Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenLastName_IfInvalid_ShouldReturnFalse(){
+        UserRegistration registration = new UserRegistration();
+        boolean result=registration.checkLastName("mA@n");
+        Assert.assertFalse(result);
     }
 
     @Test
@@ -34,6 +48,13 @@ public class UserRegistrationTest {
     }
 
     @Test
+    public void givenMobileNumber_IfInvalid_ShouldReturnFalse(){
+        UserRegistration registration = new UserRegistration();
+        boolean result=registration.checkMobileNumber("919411368453");
+        Assert.assertFalse(result);
+    }
+
+    @Test
     public void givenPassword_IfValid_ShouldReturnTrue(){
         UserRegistration registration = new UserRegistration();
         boolean result=registration.checkPassword("a*Kbs9dg&Vo");
@@ -41,7 +62,7 @@ public class UserRegistrationTest {
     }
 
     @Test
-    public void givenPassword_IfValid_ShouldReturnFalse(){
+    public void givenPassword_IfInvalid_ShouldReturnFalse(){
         UserRegistration registration = new UserRegistration();
         boolean result=registration.checkPassword("a*bsKdg&Vo");
         Assert.assertFalse(result);
